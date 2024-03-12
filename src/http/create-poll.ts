f2dfaa2f72702135dcbfc7d6fe2fa1b0d8d83dfc
Ryan Sakurai <ryansakurai03@gmail.com>
2024-03-12 19:10:35 -0300

    refactor createPoll()

--- src/http/create-poll.ts
+++ src/http/create-poll.ts
@@ -1,15 +1,15 @@
-import { z } from "zod"
-import { prisma } from "../lib/prisma"
-import { FastifyInstance } from "fastify"
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
 
 export async function createPoll(app: FastifyInstance) {
     app.post("/polls", async (request, reply) => {
-        const createPollBody = z.object({
+        const pollBodyType = z.object({
             title: z.string(),
-            options: z.array(z.string())
-        })
+            options: z.array(z.string()),
+        });
 
-        const { title, options } = createPollBody.parse(request.body)
+        const { title, options } = pollBodyType.parse(request.body);
 
         const poll = await prisma.poll.create({
             data: {
@@ -17,13 +17,13 @@ export async function createPoll(app: FastifyInstance) {
                 options: {
                     createMany: {
                         data: options.map(optionTitle => {
-                            return { title: optionTitle }
-                        })
-                    }
-                }
-            }
-        })
+                            return { title: optionTitle };
+                        }),
+                    },
+                },
+            },
+        });
 
-        return reply.status(201).send(poll)
-    })
+        return reply.status(201).send(poll);
+    });
 }
