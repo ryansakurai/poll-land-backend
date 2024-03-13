33d725efb8902a38e1a00603307e06eec9ffedc6
Ryan Sakurai <ryansakurai03@gmail.com>
2024-03-12 22:45:08 -0300

    add semicolons to get-poll

--- src/http/get-poll.ts
+++ src/http/get-poll.ts
@@ -1,15 +1,15 @@
-import { z } from "zod"
-import { prisma } from "../lib/prisma"
-import { FastifyInstance } from "fastify"
-import { redis } from "../lib/redis"
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { redis } from "../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
     app.get("/polls/:pollId", async (request, reply) => {
-        const getPollParams = z.object({
-            pollId: z.string().uuid()
-        })
+        const pollParamType = z.object({
+            pollId: z.string().uuid(),
+        });
 
-        const { pollId } = getPollParams.parse(request.params)
+        const { pollId } = pollParamType.parse(request.params);
 
         const poll = await prisma.poll.findUnique({
             where: {
@@ -23,19 +23,19 @@ export async function getPoll(app: FastifyInstance) {
                     },
                 },
             },
-        })
+        });
 
-        if (!poll)
-            return reply.status(400).send({ message: "Poll not found." })
+        if(!poll)
+            return reply.status(400).send({ message: "Poll not found." });
 
-        const rawVoteList = await redis.zrange(pollId, 0, -1, "WITHSCORES")
+        const rawVoteList = await redis.zrange(pollId, 0, -1, "WITHSCORES");
         const voteList = rawVoteList.reduce((outputObj, element, idx) => {
             if (idx % 2 == 0) {
-                const score = rawVoteList[idx + 1]
-                Object.assign(outputObj, { [element]: Number(score) })
+                const score = rawVoteList[idx + 1];
+                Object.assign(outputObj, { [element]: Number(score) });
             }
-            return outputObj
-        }, {} as Record<string, number>)
+            return outputObj;
+        }, {} as Record<string, number>);
 
         return reply.status(200).send({
             poll: {
@@ -46,9 +46,9 @@ export async function getPoll(app: FastifyInstance) {
                         id: option.id,
                         title: option.title,
                         score: option.id in voteList ? voteList[option.id] : 0,
-                    }
+                    };
                 }),
-            }
-        })
-    })
+            },
+        });
+    });
 }
