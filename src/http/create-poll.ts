7f86511e6e73c6bae2d45bf1745ca516e48eafad
Ryan Sakurai <ryansakurai03@gmail.com>
2024-06-11 20:03:27 -0300

    refactor create-poll

--- src/http/create-poll.ts
+++ src/http/create-poll.ts
@@ -2,23 +2,22 @@ import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
+const bodyType = z.object({
+    title: z.string(),
+    options: z.array(z.string()),
+});
+
 // TODO: std errors
 const createPoll = async (app: FastifyInstance) => {
     app.post("/polls", async (request, reply) => {
-        const bodyType = z.object({
-            title: z.string(),
-            options: z.array(z.string()),
-        });
-
-        const result = bodyType.safeParse(request.body);
-        if(!result.success) {
+        const bodyParseReturn = bodyType.safeParse(request.body);
+        if(!bodyParseReturn.success) {
             return reply.status(422).send({
                 code: "invalidBody",
                 details: "Request body is in incorrect format.",
             });
         }
-
-        const { title, options } = result.data;
+        const { title, options } = bodyParseReturn.data;
 
         const poll = await prisma.poll.create({
             data: {
