e5180a8c2b39cb6ffaf2ca96a4f23f3f9e7fd248
Ryan Sakurai <ryansakurai03@gmail.com>
2024-06-11 20:33:51 -0300

    add safe parse to get-live-poll-results

--- src/web-socket/get-live-poll-results.ts
+++ src/web-socket/get-live-poll-results.ts
@@ -2,13 +2,23 @@ import { FastifyInstance } from "fastify";
 import z from "zod";
 import { votesPubSub } from "../utils/votes-pub-sub";
 
+
+const paramType = z.object({
+    pollId: z.string().uuid(),
+});
+
 const getLivePollResults = async (app: FastifyInstance) => {
     app.get("/polls/:pollId/liveResults", { websocket: true }, (connection, request) => {
-        const paramType = z.object({
-            pollId: z.string().uuid(),
-        });
-
-        const { pollId } = paramType.parse(request.params);
+        const paramParseReturn = paramType.safeParse(request.params);
+        if(!paramParseReturn.success) {
+            connection.socket.send(JSON.stringify({
+                code: "invalidParams",
+                details: "Request parameters are in incorrect format.",
+            }));
+            connection.socket.close();
+            return;
+        }
+        const { pollId } = paramParseReturn.data;
 
         votesPubSub.subscribe(pollId, (message) => {
             connection.socket.send(JSON.stringify(message));
